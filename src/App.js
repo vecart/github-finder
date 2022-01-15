cce0b8cb5385418e5be77337a304c980acc4d995
Scott Barona <vecartphoto@gmail.com>
2022-01-15 16:09:02 -0500

    component and layout folders created - Navbar.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
   render() {
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className='App'>
+        <h1>Hello</h1>
       </div>
     );
   }
-  
 }
 
 export default App;
