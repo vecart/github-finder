b51bfd155247703936ccdbf5d2cd8e1d72d71c93
Scott Barona <vecartphoto@gmail.com>
2022-01-17 13:07:34 -0500

    refactoring Navbar and UserItems components to stateless functional components passing destructured props as parameters to clean up code; also added propTypes to UserItems

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class Navbar extends Component {
+const Navbar = ({ title, icon }) => {
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+        </nav>
+    )
+};
 
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    };
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+};
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    };
-
-    render() {
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    <i className={this.props.icon} /> {this.props.title}
-                </h1>
-            </nav>
-        )
-    }
-}
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
 
 export default Navbar
