64a4ec8cedf4370847f9c696e599a5f82750cae3
Scott Barona <vecartphoto@gmail.com>
2022-01-17 13:37:28 -0500

    adding componentDidMount lifecycle method and installed/imported axios to get users from githubAPI users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log(123)
+  }
+
   render() {
     return (
       <div className='App'>
