3b87fb43ddb463b25ee48f879d06ddcc3dd8da8e
Scott Barona <vecartphoto@gmail.com>
2022-01-18 17:01:21 -0500

    passed prop up from Search component and caught as prop in App.js render method, creating new function to pass prop, and updating api call to search user specified on the onSubmit handler text, added function propType in Search component for searchUsers function.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
@@ -11,12 +12,12 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
+  searchUsers = async text => {
     this.setState({ loading: true })
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
-    this.setState({ users: res.data, loading: false})
+    this.setState({ users: res.data.items, loading: false});
   }
 
   render() {
@@ -24,8 +25,8 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="className container">
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>       
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     state = {
         text: ''
+    };
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
     }
 
     onSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.text)
-    }
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: '' });
+    };
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
@@ -31,7 +37,7 @@ export class Search extends Component {
                 </form> 
             </div>
         )
-    }
+    };
 }
 
 export default Search
