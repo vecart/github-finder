13343cdec8a36931d83460672678bad62bc22dd5
Scott Barona <vecartphoto@gmail.com>
2022-01-18 18:02:26 -0500

    destructured users and loading in App component

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ class App extends Component {
   
 
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className='App'>
         <Navbar />
@@ -31,9 +33,9 @@ class App extends Component {
           <Search 
             searchUsers={this.searchUsers} 
             clearUsers={this.clearUsers} 
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>       
       </div>
     );
