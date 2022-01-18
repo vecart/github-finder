b038247815c6e56d82bd4b0c6d51c1591c201eed
Scott Barona <vecartphoto@gmail.com>
2022-01-18 18:53:10 -0500

    Alert set as prop in onSubmit handler, added to propTypes in Search component, pass setAlert as prop in App component and set functionality with timeout so alert is removed after 5s.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 
 import './App.css';
@@ -9,8 +10,9 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
-  }
+    loading: false,
+    alert: null
+  };
 
   searchUsers = async text => {
     this.setState({ loading: true })
@@ -18,22 +20,29 @@ class App extends Component {
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
     this.setState({ users: res.data.items, loading: false});
-  }
+  };
 
   clearUsers = () => this.setState({ users: [], loading: false });
   
+  setAlert = (msg, type) => {
+    this.setState({ alert: {msg, type} })
+
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
 
     return (
       <div className='App'>
         <Navbar />
         <div className="className container">
+          <Alert alert={alert}/>
           <Search 
             searchUsers={this.searchUsers} 
             clearUsers={this.clearUsers} 
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>       
